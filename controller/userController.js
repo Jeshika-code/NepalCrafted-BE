7b4791ae87fc8870b2c85faa33d7081b1d155885
Jeshika-code <jeshikasapkota5@gmail.com>
2023-12-22 22:23:28 +0545

    reset password

--- controller/userController.js
+++ controller/userController.js
@@ -72,11 +72,12 @@ exports.forgotPassword = catchAsyncError(async (req, res, next) => {
   const resetToken = user.getResetPasswordToken();
 
   await user.save({ validateBeforeSave: false });
-  const resetPasswordUrl = `${req.protocol}://${req.get(
-    "host"
-  )}/api/v1/password/reset/${resetToken}`;
+  // const resetPasswordUrl = `${req.protocol}://${req.get(
+  //   "host"
+  // )}/api/v1/password/reset/${resetToken}`;
+  const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`; 
 
-  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then, please ignore and don't click the reset link.`;
+  const message = `Your password reset token is HHH:- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then, please ignore and don't click the reset link.`;
 
   try {
     await sendEmail({
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,15 +1,14 @@
-//Error in this file 
 const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
     host: process.env.SMPT_HOST,
     port: process.env.SMPT_PORT,
-    service: process.env.SMPT_SERVICE, 
+    service: process.env.SMPT_SERVICE,
     secure:true,
     logger:true,
     debug:true,
-    secureConnection:false,        
+    secureConnection:false,
     auth: {
       user: process.env.SMPT_MAIL,
       pass: process.env.SMPT_PASSWORD,
